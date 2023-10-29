0b77290d678a5207d422bebdb3207a453fea897b
Al Zee <z@alz.ee>
2023-10-29 21:59:41 +0800

    configureFileds in curd Node

--- src/Controller/Admin/NodeCrudController.php
+++ src/Controller/Admin/NodeCrudController.php
@@ -4,6 +4,15 @@ namespace App\Controller\Admin;
 
 use App\Entity\Node;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use App\Admin\Field\VichImageField;
 
 class NodeCrudController extends AbstractCrudController
 {
@@ -12,14 +21,25 @@ class NodeCrudController extends AbstractCrudController
         return Node::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield AssociationField::new('lawyer');
+        yield AssociationField::new('type');
+        yield TextField::new('title');
+        yield ImageField::new('application')
+            ->onlyOnIndex()
+            ->setBasePath('img/node/thumbnail/')
+            // ->setUploadDir('public/img/node/')
+        ;
+        yield VichImageField::new('applicationImageFile', 'applicationImageFile')
+            ->hideOnIndex()
+            ;
+        yield TextareaField::new('body')
+            ->onlyOnForms()
+            // ->addCssClass('test')
+            ;
+        yield TextField::new('others');
+        yield DateTimeField::new('createdAt')
+            ->onlyOnIndex();
     }
-    */
 }
