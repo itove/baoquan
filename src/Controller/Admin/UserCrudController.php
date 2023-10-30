ec86b62bf24e48e0d5fb3cbc53de63fa7bb8e5ee
Al Zee <z@alz.ee>
2023-10-30 15:22:35 +0800

    fields in crud User

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,14 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +20,19 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id')->onlyOnIndex();
+        yield TextField::new('username');
+        yield TextField::new('phone');
+        yield TextField::new('openid')->onlyOnIndex();
+        yield TextField::new('name');
+        // yield TextField::new('avatar');
+        yield ChoiceField::new('roles')
+            ->setChoices(['admin' => 'ROLE_ADMIN'])
+            ->allowMultipleChoices()
+        ;
+        yield AssociationField::new('firm');
+        yield DateTimeField::new('createdAt')->onlyOnIndex();
     }
-    */
 }
--- src/Entity/Firm.php
+++ src/Entity/Firm.php
@@ -35,6 +35,11 @@ class Firm
         $this->users = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->name;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
