6e1adfb5909fd738dfd67290dc900a603857a4bf
Al Zee <z@alz.ee>
2023-10-31 20:35:29 +0800

    expose type as api

--- src/Entity/Type.php
+++ src/Entity/Type.php
@@ -4,8 +4,17 @@ namespace App\Entity;
 
 use App\Repository\TypeRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 
 #[ORM\Entity(repositoryClass: TypeRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read']],
+    denormalizationContext: ['groups' => ['write']],
+)]
 class Type
 {
     #[ORM\Id]
@@ -14,6 +23,7 @@ class Type
     private ?int $id = null;
 
     #[ORM\Column(length: 15)]
+    #[Groups(['read'])]
     private ?string $name = null;
 
     public function __toString()
