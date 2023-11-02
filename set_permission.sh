08f670f0fafa4e85f0c43f2cb864a02bcb836fb7
Al Zee <z@alz.ee>
2023-11-02 10:53:14 +0800

    nth

--- set_permission.sh
+++ set_permission.sh
@@ -10,6 +10,3 @@
 find public/files/ -type d -exec sudo chmod 775 {} \;
 
 find public/files/ -type f -exec sudo chmod 664 {} \;
-
-sudo chmod 775 public/files/media/
-sudo chmod 775 public/files/media/thumbnail/
