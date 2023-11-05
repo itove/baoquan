e1b65a24f8c493213ead207bcb9e98cdb6baf909
Al Zee <z@alz.ee>
2023-11-05 11:01:13 +0800

    nth

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\Wx;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/api')]
 class ApiController extends AbstractController
